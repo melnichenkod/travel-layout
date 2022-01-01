165e266833da5fc67926eb93f30b12b982ac152c
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2022-01-01 20:47:50 +0200

    added ability added menu hiding by click

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,12 @@
 new Gallery(document.getElementById('gallery'), {
   margin: 10
 })
+
+/* Burger */
+const MENU_OPENED_CLASS = 'burger-menu_opened';
+const burgerNode = document.querySelector('.burger-menu');
+const burgerControlNode = document.querySelector('.burger-menu__control');
+
+burgerControlNode.addEventListener('click', (event) => {
+  burgerNode.classList.toggle(MENU_OPENED_CLASS);
+})
